b70e680e4f86bf98576de8925eefc73fcf4b82e9
Devendra <amritkandel57@gmail.com>
2025-01-01 12:06:07 +0545

    NEW FILE ADDED

--- array.js
+++ array.js
@@ -1,17 +1,75 @@
 //function that adds the number 7 at the end of the array using push method
-function addSevenToArray(arr){
-arr.push(7);
-return arr;
+function addSevenToArray(arr) {
+    arr.push(7);
+    return arr;
 
 }
-let myArray=[1,2,3];
-let updatedArray=addSevenToArray(myArray);
+let myArray = [1, 2, 3];
+let updatedArray = addSevenToArray(myArray);
 console.log(updatedArray);
 //function to remove using the pop method
-function removeLastElement(arr){
-    arr.pop("mango");
-    return arr;}
-    let newArray=["bannana","apple","mango"];
-    let updatedArray2=removeLastElement(newArray);
-    console.log(updatedArray2);
-    
+function removeLastElement(arr) {
+    arr.pop();
+    return arr;
+}
+let newArray = ["bannana", "apple", "mango"];
+let updatedArray2 = removeLastElement(newArray);
+console.log(updatedArray2);
+
+// using shift method to remove the first elemment
+
+function removeFirstElement(arr) {
+    arr.shift();
+    return arr;
+}
+let fruits = [10, 20, 30, 40];
+let updatedArray3 = removeFirstElement(fruits);
+console.log(updatedArray3);
+// using unshift method to add the element at the beginning of the array
+function addElementAtBeginning(arr) {
+    arr.unshift("start");
+    return arr;
+
+
+}
+let newArray2 = ["middle", "end"]
+let updatedArray4 = addElementAtBeginning(newArray2);
+console.log(updatedArray4);
+
+
+//converting array into string
+let number=["one","two","three","four"];
+let string = number.toString();
+console.log(string);
+
+//Combining two array into one using concatenation
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+let combinedArray = array1.concat(array2);
+console.log(combinedArray);
+
+//using slice method to extract first two elements from the array
+function extractFirstTwoElements(arr) {
+    return arr.slice(0, 2);}
+    let numbers = [1, 2, 3, 4, 5];
+    let extractedArray = extractFirstTwoElements(numbers);
+    console.log(extractedArray);
+
+    //using toStrng method to convert array into string and then finding its length
+    function convertToArrayStringAndFindLength(arr) {
+        let string = arr.toString();
+        return string.length;
+    }
+    let numbers2 = [1, 2, 3, 4, 5];
+    let length = convertToArrayStringAndFindLength(numbers2);
+    console.log(length);
+
+    //using pop method to remove last element from the array
+    function removeLastElement(arr) {
+        arr.pop();
+        return arr}
+        let numbers3 = [1, 2, 3, 4, 5];
+        let updatedArray5 = removeLastElement(numbers3);
+        console.log(updatedArray5);
+
+        //
---
+++ conditionalStatement.js
@@ -0,0 +1,33 @@
+let age=18;
+if(age>18){
+
+console.log("congratulation you are adult...ENJOYEE");
+
+}
+else{
+    console.log("SORRY SORREY");
+}
+//program to find wether number is positive or negative
+let number=0;
+if(number>0){
+    console.log("number is positive");}
+    else {
+
+        console.log("number is negative");
+    }
+    //program to check wether a number is even or odd
+    let number1=234;
+    if(number1%2==0){
+        console.log("number is even");}
+        else {
+            console.log("number is odd");
+        }
+        // program to find wether a given char is vowel or consonant
+        let char="d";
+        if(char=="a"||char=="e"||char=="i"||char=="o"){
+            console.log("char is vowel");
+        }
+        else {
+            console.log("char is consonant");
+        }
+
